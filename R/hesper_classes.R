8ceecac0dc693f0d797e6851306cd31dc60c0f80
gnoblet <gnoblet@zaclys.net>
2025-08-04 11:42:18 +0200

    Add tests to SL

--- R/hesper_classes.R
+++ R/hesper_classes.R
@@ -1,27 +1,3 @@
-#' S7 class for skip logic (SL)
-#'
-#' @field hesper_var Name of the HESPER variable (character).
-#' @field subset_var Name of the subset variable (character).
-#' @field subset_vals Values in the subset variable (character).
-#'
-#' @details
-#' The `SL` class is designed to encapsulate the properties of a skip logic rule in HESPER. It includes the name of the HESPER variable, the name of the subset variable, and the values in the subset variable.
-#' The class validates that:
-#' - The `hesper_var` property is a valid HESPER variable.
-#' - The `subset_var` property is a valid variable name.
-#' - The `subset_vals` property contains valid values for the subset variable.
-#'
-#' @export
-SL <- S7::new_class(
-  "SL",
-  properties = list(
-    hesper_var = S7::class_character,
-    subset_var = S7::class_character,
-    subset_vals = S7::class_character
-  )
-)
-
-
 #' S7 class for hespeR data.frames (enhanced)
 #'
 #' Inherits from HesperDefault. May have additional properties or methods.
