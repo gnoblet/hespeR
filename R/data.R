d4cdb88a9e8c9c4a472a29e74b6a29f5ab92ba1c
gnoblet <gnoblet@zaclys.net>
2025-08-22 17:19:07 +0200

    New approach to hesper_vars() and hesper_opts() and tests

--- R/data.R
+++ R/data.R
@@ -2,15 +2,17 @@
 #'
 #' A character vector listing all allowed HESPER item columns for use in hespeR data structures and validation or if needed to be accessed easily.
 #'
-#' @typed none: empty
-#'  No input parameters.
+#' @typed ...: character[0,29]
+#'  Optional variable names to validate. If none provided, all allowed variables are returned.
 #'
 #' @typedreturn character[29]
 #'  All allowed HESPER item names.
 #'
 #' @export
-hesper_vars <- function() {
-  c(
+hesper_vars <- function(...) {
+  #------ Possible values
+
+  allowed_vars <- c(
     "hesper_drinking_water",
     "hesper_food",
     "hesper_shelter",
@@ -41,24 +43,114 @@ hesper_vars <- function() {
     "hesper_care_community",
     "hesper_other"
   )
+
+  #------ Checks
+
+  vars <- list(...)
+
+  # no duplicates allowed in ...
+  if (length(vars) > 0) {
+    # Check for duplicates
+    if (any(duplicated(vars))) {
+      dupes <- unique(vars[duplicated(vars)])
+      rlang::abort(c(
+        "Duplicated variable names provided",
+        "i" = glue::glue(
+          "Duplicated variable(s) found: ",
+          glue::glue_collapse(dupes, sep = ", ")
+        )
+      ))
+    }
+  }
+
+  # all elements are character strings
+  checkmate::assert_list(vars, types = "character", max.len = 29)
+
+  # all elements are in allowed_vars
+  if (length(vars) > 0) {
+    not_allowed <- setdiff(unlist(vars), allowed_vars)
+    if (length(not_allowed) > 0) {
+      rlang::abort(c(
+        "Invalid variable names provided",
+        "i" = glue::glue(
+          "Invalid variable(s) found: ",
+          glue::glue_collapse(not_allowed, sep = ", ")
+        ),
+        "*" = "Run hesper_vars() to get all allowed variables."
+      ))
+    }
+  }
+
+  #----- Get vars
+
+  if (length(vars) == 0) {
+    return(allowed_vars)
+  } else {
+    return(unlist(vars))
+  }
 }
 
 #' All allowed response options for HESPER items
 #'
 #' This character vector contains all allowed response options for HESPER items, which can be used for validation or reference in the hespeR package These are: 'serious_problem', 'no_serious_problem', 'dnk', 'pnta', and 'not_applicable'.
-#' @typed none: empty
-#'  No input parameters.
+#'
+#' @typed ...: character[0,5]
+#'  Optional options to validate. If none provided, all allowed options are returned.
 #'
 #' @typedreturn character[5]
 #'  All allowed HESPER response options.
 #'
 #' @export
-hesper_opts <- function() {
-  c(
+hesper_opts <- function(...) {
+  #------ Possible values
+  allowed_opts <- c(
     "serious_problem",
     "no_serious_problem",
     "dnk",
     "pnta",
     "not_applicable"
   )
+
+  #------ Checks
+
+  opts <- list(...)
+  # no duplicates allowed in ...
+  if (length(opts) > 0) {
+    # Check for duplicates
+    if (any(duplicated(opts))) {
+      dupes <- unique(opts[duplicated(opts)])
+      rlang::abort(c(
+        "Duplicated options provided",
+        "i" = glue::glue(
+          "Duplicated option(s) found: ",
+          glue::glue_collapse(dupes, sep = ", ")
+        )
+      ))
+    }
+  }
+
+  # all elements are character strings
+  checkmate::assert_list(opts, types = "character", max.len = 5)
+
+  # all elements are in allowed_opts
+  if (length(opts) > 0) {
+    not_allowed <- setdiff(unlist(opts), allowed_opts)
+    if (length(not_allowed) > 0) {
+      rlang::abort(c(
+        "Invalid options provided",
+        "i" = glue::glue(
+          "Invalid option(s) found: ",
+          glue::glue_collapse(not_allowed, sep = ", ")
+        ),
+        "*" = "Run hesper_opts() to get all allowed options."
+      ))
+    }
+  }
+
+  #----- Get opts
+  if (length(opts) == 0) {
+    return(allowed_opts)
+  } else {
+    return(unlist(opts))
+  }
 }
